67122379b5df905fe45b7ac587418dba81eb13be
Mathianga <gabrielmathiang1@gmail.com>
2023-06-25 01:23:33 -0700

    updated

---
+++ printf_oct.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * printf_oct - a function that prints an octal number.
+ * @val: arguments to be pass to the function.
+ * Return: counter.
+ */
+
+int printf_oct(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int number = va_arg(val, unsigned int);
+	unsigned int temp = number;
+
+	while (number / 8 != 0)
+	{
+		number /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+
+	free(array);
+	return (counter);
+}
