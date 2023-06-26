84cbdf138d0190ca4a7d7f605d0a6a87ea45f226
Abbaaji12 <mohammedajimustapha@gmail.com>
2023-06-26 15:57:37 -0700

    update on printf_bin.c

--- printf_bin.c
+++ printf_bin.c
@@ -10,19 +10,19 @@ int printf_bin(va_list val)
 {
 	int flag = 0;
 	int cont = 0;
-	int i, a = 1, j;
+	int m, a = 1, n;
 	unsigned int number = va_arg(val, unsigned int);
 	unsigned int p;
 
-	for (i = 0; i < 32; i++)
+	for (m = 0; m < 32; m++)
 	{
-		p = ((a << (31 - i)) & number);
-		if (p >> (31 - i))
+		p = ((a << (31 - m)) & number);
+		if (p >> (31 - m))
 			flag = 1;
 		if (flag)
 		{
-			j = p >> (31 - i);
-			_putchar(j + 48);
+			n = p >> (31 - m);
+			_putchar(n + 48);
 			cont++;
 		}
 	}
