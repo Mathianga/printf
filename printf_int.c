4222cf58adeef2f32ba5b527759d10fb5ee1dc83
Abbaaji12 <mohammedajimustapha@gmail.com>
2023-06-26 15:06:12 -0700

    update on printf_int

--- printf_int.c
+++ printf_int.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * printf_int - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ * printf_int -this  prints an integer
+ * @args: arguments to be printed
+ * Return: number of characters are also  printed
  */
 int printf_int(va_list args)
 {
 	int n = va_arg(args, int);
 	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int  m = 1;
 
 	n = n / 10;
 	num = n;
@@ -19,7 +19,7 @@ int printf_int(va_list args)
 		num = -num;
 		n = -n;
 		last = -last;
-		i++;
+		m++;
 	}
 	if (num > 0)
 	{
@@ -35,10 +35,10 @@ int printf_int(va_list args)
 			_putchar(digit + '0');
 			num = num - (digit * exp);
 			exp = exp / 10;
-			i++;
+			m++;
 		}
 	}
 	_putchar(last + '0');
 
-	return (i);
+	return (m);
 }
