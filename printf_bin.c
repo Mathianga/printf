d26219c467f5b281adbdd98247add4a1f8b62445
Mathianga <gabrielmathiang1@gmail.com>
2023-06-27 12:53:28 -0700

    updated

--- printf_bin.c
+++ printf_bin.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * printf_bin - prints a binary number.
- * @val: arguments to be pass.
+ * printf_bin - a function that prints a binary number.
+ * @val: arguments to be pass in the function.
  * Return: 1.
  */
 
@@ -10,26 +10,28 @@ int printf_bin(va_list val)
 {
 	int flag = 0;
 	int cont = 0;
-	int m, a = 1, n;
-	unsigned int number = va_arg(val, unsigned int);
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
 	unsigned int p;
 
-	for (m = 0; m < 32; m++)
+	for (i = 0; i < 32; i++)
 	{
-		p = ((a << (31 - m)) & number);
-		if (p >> (31 - m))
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
 			flag = 1;
 		if (flag)
 		{
-			n = p >> (31 - m);
-			_putchar(n + 48);
+			b = p >> (31 - i);
+			_putchar(b + 48);
 			cont++;
 		}
 	}
+
 	if (cont == 0)
 	{
 		cont++;
 		_putchar('0');
 	}
+
 	return (cont);
 }
